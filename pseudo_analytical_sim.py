f995ad4bca7b43d44f8dd40fbda86d2686f7ccec
dan <dansturm@uw.edu>
2023-02-22 18:10:14 -0800

    pushing now in case of some massive screw up

--- pseudo_analytical_sim.py
+++ pseudo_analytical_sim.py
@@ -22,7 +22,6 @@ class hardware_state():
 		self.SRAM_input_size = hardware_state_info.loc["SRAM Input Size"].item()
 		self.SRAM_filter_size = hardware_state_info.loc["SRAM Filter Size"].item()
 		self.SRAM_output_size = hardware_state_info.loc["SRAM Output Size"].item()
-		self.accumulator_elements = hardware_state_info.loc["Accumulator Elements"].item()
 		self.batch_size = hardware_state_info.loc["Batch Size"].item()
 
 		'''
@@ -39,7 +38,9 @@ class hardware_state():
 		'''
 		self.input_SRAM  = SRAM_model.SRAM_model(self.SRAM_input_size, "input")
 		self.filter_SRAM = SRAM_model.SRAM_model(self.SRAM_filter_size, "filter")
-	 
+	 	self.output_SRAM = SRAM_model.SRAM_model(self.SRAM_output_size, "output")
+
+
 	def set_NN(self, NN_layers_all):
 		self.NN_layers_all = NN_layers_all
 		self.num_NN_layers = len(NN_layers_all)
@@ -53,14 +54,21 @@ class hardware_state():
 		self.SRAM_input_reads = [0] * self.num_NN_layers
 		self.SRAM_filter_reads = [0] * self.num_NN_layers
 		self.SRAM_output_writes = [0] * self.num_NN_layers
+		self.SRAM_output_reads = [0] * self.num_NN_layers
+
 		self.DRAM_input_reads = [0] * self.num_NN_layers
 		self.DRAM_filter_reads = [0] * self.num_NN_layers
 		self.DRAM_output_writes = [0] * self.num_NN_layers
+		self.DRAM_output_reads = [0] * self.num_NN_layers
+
 		self.SRAM_DRAM_input_misses = [0] * self.num_NN_layers
 		self.SRAM_DRAM_filter_misses = [0] * self.num_NN_layers
+		self.SRAM_DRAM_output_misses = [0] * self.num_NN_layers
+
 		self.SRAM_input_accesses = [0] * self.num_NN_layers
 		self.SRAM_filter_accesses = [0] * self.num_NN_layers
-		self.accumulator_dumps = [0] * self.num_NN_layers
+		self.SRAM_output_accesses = [0] * self.num_NN_layers
+
 
 	def run_all_layers(self):
 		print("\nBeginning analytical modeling simulation")
@@ -109,12 +117,18 @@ class hardware_state():
 		xStride     = NN_layer["X Stride"]
 		yStride     = NN_layer["Y Stride"]
 
-		filter_size = filter_rows * filter_cols * channels
-		single_input_size = input_cols * input_rows * channels
 
-		overall_col_fold = math.ceil(num_filter / self.array_cols)  
-		overall_row_fold = math.ceil(filter_size / self.array_rows)
-		overall_batch_fold = self.batch_size
+
+		input_divider = 2
+		col_divider_for_output = 2
+
+		filter_size = filter_rows * filter_cols * channels
+		single_input_size = round(input_cols * input_rows * channels / input_divider)
+
+		col_fold = math.ceil(num_filter / self.array_cols)  
+		row_fold = math.ceil(filter_size / self.array_rows)
+		#input_fold = math.ceil(self.batch_size * input_divider)
+		input_fold = math.ceil(input_cols * input_rows * channels * self.batch_size / single_input_size)
 
 		conv_rows = math.ceil((input_rows - filter_rows) / xStride) + 1
 		conv_cols = math.ceil((input_cols - filter_cols) / yStride) + 1
@@ -122,137 +136,107 @@ class hardware_state():
 		#conv_cols = math.ceil(input_cols / stride)
 
 		num_conv_in_input = conv_rows * conv_cols 
-		global num_conv_in_input_delete 
-		num_conv_in_input_delete = num_conv_in_input
-		batch_col_fold_product_allowed = self.accumulator_elements / num_conv_in_input # what can fit onto accumulator
-		if (batch_col_fold_product_allowed < 1):
-			print("CANNOT DO SIMULATION B/C ACCUMULATOR NOT BIG ENOUGH FOR ONE BATCH WITH ONE COL FOLD")
-			print("Current Layer: ", self.current_layer)
-			return(-1)
+		#global num_conv_in_input_delete 
+		#num_conv_in_input_delete = num_conv_in_input
+
+		self.num_compute_cycles_theory[self.current_layer] = self.batch_size *  num_conv_in_input * col_fold * row_fold 
+		self.SRAM_input_reads[self.current_layer] = self.batch_size * num_conv_in_input * filter_size * col_fold
+
+		self.SRAM_filter_reads[self.current_layer] = filter_size * num_filter # this could be a problem, depends on order, right? 
+		# like need to multiply by batch size if input isn't on the very inside 
 
 
-		minor_col_fold = 1
-		minor_batch_fold = math.floor(batch_col_fold_product_allowed / minor_col_fold)
-		if minor_batch_fold > overall_batch_fold:
-			minor_batch_fold = overall_batch_fold
-			minor_col_fold = math.floor(batch_col_fold_product_allowed / minor_batch_fold)
-			if minor_col_fold > overall_col_fold:
-				minor_col_fold = overall_col_fold
-		'''
-		minor_batch_fold = 1
-		minor_col_fold = math.floor(batch_col_fold_product_allowed / minor_batch_fold)
-		if minor_col_fold > overall_col_fold:
-			minor_col_fold = overall_col_fold
-		'''
-		minor_row_fold = 1
-
-		major_col_fold = math.ceil(overall_col_fold / minor_col_fold)
-		major_row_fold = math.ceil(overall_row_fold / minor_row_fold)
-		major_batch_fold = math.ceil(overall_batch_fold / minor_batch_fold)
-
-		self.accumulator_dumps[self.current_layer] = major_batch_fold * major_col_fold
-		self.num_compute_cycles_theory[self.current_layer] = self.batch_size * overall_col_fold * overall_row_fold * num_conv_in_input
-		self.SRAM_input_reads[self.current_layer] = num_conv_in_input * self.batch_size * filter_size * overall_col_fold
-		self.SRAM_filter_reads[self.current_layer] = filter_size * num_filter * major_batch_fold
-		self.SRAM_output_writes[self.current_layer] = num_conv_in_input * self.batch_size * num_filter
-		if self.current_layer == (self.num_NN_layers - 1):
-			self.DRAM_output_writes[self.current_layer] = self.SRAM_output_writes[self.current_layer]
-		else:
-			self.DRAM_output_writes[self.current_layer] = max(self.SRAM_output_writes[self.current_layer] - self.SRAM_output_size, 0)
+		self.SRAM_output_writes[self.current_layer] = num_conv_in_input * self.batch_size * num_filter * row_fold 
+		self.SRAM_output_reads[self.current_layer] = num_conv_in_input * self.batch_size * num_filter * row_fold
 
 		if self.current_layer == 0:
 			SRAM_input_output_crossover_data = 0
 		else:
 			SRAM_input_output_crossover_data = min(self.SRAM_output_size, self.SRAM_output_writes[self.current_layer - 1])
 
-		new_mem = self.input_SRAM.new_layer(single_input_size, self.batch_size, SRAM_input_output_crossover_data)
+		new_mem = self.input_SRAM.new_layer(single_input_size, input_fold, SRAM_input_output_crossover_data)
 		if new_mem == -1:
 			return -1
-		new_mem = self.filter_SRAM.new_layer(self.array_cols * self.array_rows, overall_row_fold * overall_col_fold, 0)
+		new_mem = self.filter_SRAM.new_layer(self.array_cols * self.array_rows, row_fold * col_fold, 0)
 		if new_mem == -1:
 			return -1
-		self.run_single_layer(major_col_fold, minor_col_fold, overall_col_fold, \
-			major_batch_fold, minor_batch_fold, overall_batch_fold,\
-					major_row_fold, minor_row_fold, overall_row_fold)
-		if (self.num_programming_theory != self.num_programming_practice):
-			print("Theoretical Programming Count NOT EQUAL to Practical Programming Count")
-			return(-1)
+		new_mem = self.output_SRAM.new_layer(num_conv_in_input * self.array_cols / col_divider_for_output, col_fold * col_divider_for_output, 0)
+		if new_mem == -1:
+			return -1
+
+
+		self.run_single_layer(col_fold, row_fold, batch_fold)
+		#if (self.num_programming_theory != self.num_programming_practice):
+		#	print("Theoretical Programming Count NOT EQUAL to Practical Programming Count")
+		#	return(-1)
+
+
+		if self.current_layer != (self.num_NN_layers - 1): 
+			self.DRAM_output_writes[self.current_layer] -= min(self.SRAM_input_size, self.SRAM_output_writes[self.current_layer])  # note that if this thing isn't very full we don't save very much 
+
+
+
+			max(self.SRAM_output_writes[self.current_layer] - self.SRAM_output_size, 0)
+
 		self.input_SRAM.conclude_layer()
 		self.filter_SRAM.conclude_layer()
+		self.output_SRAM.conclude_layer()
 
-
-
-	def run_single_layer(self, major_col_fold, minor_col_fold, overall_col_fold,\
-		major_batch_fold, minor_batch_fold, overall_batch_fold,\
-			major_row_fold, minor_row_fold, overall_row_fold):     
+	def run_single_layer(self, col_fold, row_fold, input_fold = 1):     
 		self.num_programming_practice[self.current_layer] = 0
-		self.num_programming_theory[self.current_layer] = overall_col_fold * overall_row_fold * major_batch_fold
-		if (overall_row_fold == 1) :
-			self.num_programming_theory[self.current_layer] = overall_col_fold
+		self.num_programming_theory[self.current_layer] = col_fold * row_fold * batch_fold
+		if (row_fold == 1) : # do we still need this? 
+			self.num_programming_theory[self.current_layer] = col_fold
 
 		global print_string
 		old_col_group = -1
 		old_row_group = -1
-		current_col_group = -1
-		current_row_group = -1
-		num_loop_iterations = overall_col_fold * overall_row_fold * overall_batch_fold
+
+		num_loop_iterations = col_fold * row_fold * batch_fold
 		current_loop_iteration = 0
 		target_inc = 10
 		target = target_inc
-		for major_col_group in range(major_col_fold):
-			for major_batch_group in range(major_batch_fold):
-				for minor_col_group in range(minor_col_fold):
-						for major_row_group in range(major_row_fold):
-							for minor_row_group in range(minor_row_fold):
-								for minor_batch_group in range(minor_batch_fold):
-									current_loop_iteration += 1
-									percent_done = 100 * current_loop_iteration / num_loop_iterations
-									if percent_done >= target:
-											addon = "percent done: "+ str(round(percent_done)) + "  "
-											print_string += addon
-											print(print_string, end="\r", flush=True)
-											print_string = print_string[0:len(print_string) - len(addon)]
-											target += target_inc
+		for col_group in range(col_fold):
+			for row_group in range(row_fold):
+				for input_group in range(input_fold):
+					current_loop_iteration += 1
+					percent_done = 100 * current_loop_iteration / num_loop_iterations
+					if percent_done >= target:
+						addon = "percent done: "+ str(round(percent_done)) + "  "
+						print_string += addon
+						print(print_string, end="\r", flush=True)
+						print_string = print_string[0:len(print_string) - len(addon)]
+						target += target_inc
 
-									old_col_group = current_col_group
-									old_row_group = current_row_group
+						#if (old_col_group != current_col_group) or (old_row_group != current_row_group):
+						#		self.num_programming_practice[self.current_layer] += 1
+						#self.num_compute_cycles_practice[self.current_layer] += num_conv_in_input_delete 
 
-									current_col_group = major_col_group * minor_col_fold + minor_col_group
-									current_row_group = major_row_group * minor_row_fold + minor_row_group
-									current_batch = major_batch_group * minor_batch_fold + minor_batch_group
+						#old_col_group = col_group
+						#old_row_group = row_group
 
-									if current_col_group >= overall_col_fold:
-											continue # could probably be "break" but just to play it safe do this
-									if current_row_group >= overall_row_fold:
-											continue
-									if current_batch >= overall_batch_fold:
-											continue
+						if (0):
+							print("")
+							print("Current Col Group:", col_group)
+							print("Current Row Group:", row_group)
+							print("Current Batch Group:", batch)
 
-									if (old_col_group != current_col_group) or (old_row_group != current_row_group):
-											self.num_programming_practice[self.current_layer] += 1
-									self.num_compute_cycles_practice[self.current_layer] += num_conv_in_input_delete 
+						filter_index = col_group * row_fold + row_group
+						self.manage_SRAM_DRAM_access(current_batch, filter_index, col_fold)
 
-									if (0):
-											print("")
-											print("Current Col Group:", current_col_group)
-											print("Current Row Group:", current_row_group)
-											print("Current Batch Group:", current_batch)
-
-									filter_index = current_col_group * major_row_fold * minor_row_fold + current_row_group
-									self.manage_SRAM_DRAM_access(current_batch, filter_index)
-
-		#self.SRAM_filter_reads[self.current_layer] = self.num_programming_practice[self.current_layer] * self.
+	#self.SRAM_filter_reads[self.current_layer] = self.num_programming_practice[self.current_layer] * self.
 		
 
-	def manage_SRAM_DRAM_access(self, current_batch, filter_index):
-		start_time = time.time()
+	def manage_SRAM_DRAM_access(self, input_index, filter_index, output_index):
+		#start_time = time.time()
 		self.input_SRAM.access_component(current_batch)
-		med_time = time.time()
+		#med_time = time.time()
 		self.filter_SRAM.access_component(filter_index)
-		end_time = time.time()
-		global input_sram_time, filter_sram_time
-		input_sram_time += med_time - start_time
-		filter_sram_time += end_time - med_time
+		self.output_SRAM.access_component(output_index)
+		#end_time = time.time()
+		#global input_sram_time, filter_sram_time
+		#input_sram_time += med_time - start_time
+		#filter_sram_time += end_time - med_time
 
 	def access_SRAM_data(self):
 		self.DRAM_input_reads  = self.input_SRAM.DRAM_reads
