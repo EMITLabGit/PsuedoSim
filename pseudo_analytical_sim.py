237670b0dd86f6d216034e376f810e71c161c911
dan <dansturm@uw.edu>
2024-04-15 17:14:10 -0700

    big clean up

--- pseudo_analytical_sim.py
+++ pseudo_analytical_sim.py
@@ -10,54 +10,27 @@ class hardware_state():
 	def __init__(self, compute_type, gemm_input):
 		self.compute_type = compute_type
 		self.cnn_input = not gemm_input
+		self.text_output = ""
 
 	def add_to_text_output(self, string_output):
 		self.text_output += "\n" + string_output
-		x = 1
 
 	def set_hardware(self, hardware_state_info):
 		self.array_rows = hardware_state_info.loc["Systolic Array Rows"]#.item()
 		self.array_cols = hardware_state_info.loc["Systolic Array Cols"]#.item()
 		self.SRAM_input_size = hardware_state_info.loc["SRAM Input Size"] * 1024 / 2 #.item() * 1000 / 2
-		self.SRAM_filter_size = hardware_state_info.loc["SRAM Filter Size"] * 1024 / 2 #.item() * 1000 / 2
-		self.SRAM_output_size = hardware_state_info.loc["SRAM Output Size"] * 1024 / 2 #.item() * 1000 / 2
 		self.batch_size = hardware_state_info.loc["Batch Size"]#.item()
 			
-		#self.input_SRAM  = SRAM_model.SRAM_model(self.SRAM_input_size, "input")
-		#self.filter_SRAM = SRAM_model.SRAM_model(self.SRAM_filter_size, "filter")
-		#self.output_SRAM = SRAM_model.SRAM_model(self.SRAM_output_size, "output")
-
-		self.text_output = ""
 
 	def set_NN(self, NN_layers_all):
 		self.NN_layers_all = NN_layers_all
 		self.num_NN_layers = len(NN_layers_all)
 
 	def set_results_vars(self):
-		self.num_program_compute_instance     = [0] * self.num_NN_layers
-		#self.num_compute_clock_cycles_analog  = [0] * self.num_NN_layers
-		#self.num_compute_clock_cycles_digital = [0] * self.num_NN_layers
-		self.num_compute_clock_cycles = [0] * self.num_NN_layers
-
-		self.num_program_clock_cycles         = [0] * self.num_NN_layers
-
-		self.SRAM_input_reads      = [0] * self.num_NN_layers
-		self.SRAM_filter_reads     = [0] * self.num_NN_layers
-		self.SRAM_output_writes_SS = [0] * self.num_NN_layers
-
-		self.DRAM_filter_reads     = [0] * self.num_NN_layers 
-		self.DRAM_output_writes_SS = [0] * self.num_NN_layers
-
-		self.DRAM_input_reads_analog  = [0] * self.num_NN_layers
-		self.DRAM_input_reads_digital = [0] * self.num_NN_layers
-
-		self.SRAM_output_reads_acc  = [0] * self.num_NN_layers
-		self.SRAM_output_writes_acc = [0] * self.num_NN_layers
-		self.DRAM_output_reads_acc  = [0] * self.num_NN_layers
-		self.DRAM_output_writes_acc = [0] * self.num_NN_layers
-
-		self.DRAM_input_reads_digital_SRAM_sharing = [0] * self.num_NN_layers
-		self.DRAM_output_writes_acc_SRAM_sharing   = [0] * self.num_NN_layers
+		self.num_program_compute_instance = [0] * self.num_NN_layers
+		self.num_compute_clock_cycles 	  = [0] * self.num_NN_layers
+		self.num_program_clock_cycles     = [0] * self.num_NN_layers
+		self.DRAM_input_reads  = [0] * self.num_NN_layers
 
 	def run_single_layer(self, NNLayer, hardwareArch):
 		self.num_NN_layers = 1
@@ -69,39 +42,18 @@ class hardware_state():
 		self.single_layer_set_params(NNLayer)
 
 		end_time = time.time()
-		AM_execution_time = (end_time - start_time) / 60
+		execution_time = (end_time - start_time) / 60
 		self.calculate_NN_totals()
 		final_time = time.time()
-		AM_post_process_time = (final_time - end_time) / 60,
+		post_process_time = (final_time - end_time) / 60,
 				
-		AM_results_SS_compare = self.return_specs_SS_compare()
-		AM_results_self_compare = self.return_specs_self_compare()
-		AM_results_SS_compare.loc[" "] = " "
-		AM_results_SS_compare.loc["Simulation Run Time [min]"] = AM_execution_time
-		AM_results_SS_compare.loc["Simulation Post Process Time [min]"] = AM_post_process_time
+		basic_results = self.return_basic_results()
+		basic_results.loc[" "] = " "
+		basic_results.loc["Simulation Run Time [min]"] = execution_time
+		basic_results.loc["Simulation Post Process Time [min]"] = post_process_time
 
-		return(AM_results_SS_compare, AM_results_self_compare, self.text_output)
+		return(basic_results, self.text_output)
 
-	def run_all_layers(self):
-		self.set_results_vars()
-		start_time = time.time()
-		for index, layer in enumerate(self.NN_layers_all):
-			self.current_layer = index
-			self.SRAM_carryover_data_current_layer = 0; self.SRAM_carryover_data_previous_layer = 0
-			status = self.single_layer_set_params(layer)
-			if (status == -1):
-				return
-		end_time = time.time()
-		AM_execution_time = round((end_time - start_time) / 60, 10)
-		self.calculate_NN_totals()
-		final_time = time.time()
-		AM_post_process_time = round((final_time - end_time) / 60, 10)
-		AM_results_SS_compare = self.return_specs_SS_compare()
-		AM_results_self_compare = self.return_specs_self_compare()
-		AM_results_SS_compare.loc[" "] = " "
-		AM_results_SS_compare.loc["Simulation Run Time [min]"] = AM_execution_time
-		AM_results_SS_compare.loc["Simulation Post Process Time [min]"] = AM_post_process_time
-		return(AM_results_SS_compare, AM_results_self_compare, self.text_output)
 
 	def compute_input_DRAM_access(self):
 		stride_factor_rows = min(self.filter_rows / self.x_stride, 1)
@@ -109,12 +61,14 @@ class hardware_state():
 		input_size = self.input_rows * self.input_cols * self.channels * stride_factor_cols * stride_factor_rows
 
 		if 0:#(self.SRAM_input_size >= input_size):
-			self.DRAM_input_reads_analog[self.current_layer] = input_size
-			self.DRAM_input_reads_analog = np.array(self.DRAM_input_reads_analog)
+			self.DRAM_input_reads[self.current_layer] = input_size
+			self.DRAM_input_reads = np.array(self.DRAM_input_reads)
 			#self.add_to_text_output("SRAM can fit entirety of input data")
 		else:
 			#self.add_to_text_output("SRAM canNOT fit entirety of input data")
-			self.iterate_row_col_fold()
+			self.make_repeat_access_list()
+			self.traverse_repeat_access_array()
+	
 
 	def make_repeat_access_list(self):
 		col_patterns = min(self.filter_cols % self.x_stride + 1, self.filter_cols)
@@ -209,14 +163,14 @@ class hardware_state():
 			convs_fill_SRAM = self.SRAM_free_space / new_data_per_conv
 
 			if convs_change_repeat_access < convs_fill_SRAM:
-				self.DRAM_input_reads_analog += new_data_per_conv * convs_change_repeat_access
+				self.DRAM_input_reads += new_data_per_conv * convs_change_repeat_access
 				self.SRAM_free_space -= new_data_per_conv * convs_change_repeat_access
 				absolute_conv_idx += convs_change_repeat_access
 				#print(convs_change_repeat_access)
 
 			elif convs_fill_SRAM <= convs_change_repeat_access:
 				self.SRAM_unfilled = 0
-				self.DRAM_input_reads_analog = self.SRAM_free_space + self.DRAM_input_reads_analog
+				self.DRAM_input_reads = self.SRAM_free_space + self.DRAM_input_reads
 				self.SRAM_free_space = self.SRAM_input_size
 				absolute_conv_idx += convs_fill_SRAM
 				local_base_conv_idx = absolute_conv_idx
@@ -224,10 +178,7 @@ class hardware_state():
 			#print()
 			#print()
 
-	def iterate_row_col_fold(self):
-		self.make_repeat_access_list()
-		self.traverse_repeat_access_array()
-	
+
 	def basic_operation_params(self):
 		self.ind_filter_size = self.filter_rows * self.filter_cols * self.channels
 		self.row_fold = math.ceil(self.ind_filter_size / self.array_rows)
@@ -264,10 +215,6 @@ class hardware_state():
 		self.set_NN_layer(NN_layer)
 		self.basic_operation_params()
 
-		#self.num_compute_clock_cycles_analog[self.current_layer]  = self.batch_size * self.num_conv_in_input * self.col_fold * self.row_fold
-		#self.num_compute_clock_cycles_digital[self.current_layer] = self.num_compute_clock_cycles_analog[self.current_layer] + (self.ind_filter_size - 1) % self.array_rows
-		#self.num_compute_clock_cycles_digital[self.current_layer] = self.batch_size * ((self.ind_filter_size - 1) % self.array_rows + self.num_conv_in_input) * self.row_fold * self.col_fold 
-
 		row_rem = self.ind_filter_size % self.array_rows
 		col_rem = self.num_filter % self.array_cols
 
@@ -281,82 +228,24 @@ class hardware_state():
 			x += row_rem + col_rem
 			self.num_compute_clock_cycles[self.current_layer] = x
 
-
-
 		self.num_program_compute_instance[self.current_layer]     = self.row_fold * self.col_fold
 		self.num_program_clock_cycles[self.current_layer]         = -1
-
-		self.SRAM_input_reads[self.current_layer]      = self.batch_size * self.num_conv_in_input * self.ind_filter_size * self.col_fold
-		self.SRAM_filter_reads[self.current_layer]     = self.ind_filter_size * self.num_filter
-		self.SRAM_output_writes_SS[self.current_layer] = self.batch_size * self.num_conv_in_input *  self.num_filter * self.row_fold 
-
-		self.DRAM_filter_reads[self.current_layer] = self.ind_filter_size * self.num_filter
-		self.DRAM_output_writes_SS[self.current_layer] = self.SRAM_output_writes_SS[self.current_layer]
-		
-		self.SRAM_output_reads_acc[self.current_layer] = -1
-		self.SRAM_output_writes_acc[self.current_layer] = -1
-		self.DRAM_output_reads_acc[self.current_layer] = -1
-		self.DRAM_output_writes_acc[self.current_layer] = -1
-		self.DRAM_input_reads_digital_SRAM_sharing[self.current_layer] = -1
-		self.DRAM_output_writes_acc_SRAM_sharing[self.current_layer] = -1
 	
 		self.compute_input_DRAM_access()
-		self.DRAM_input_reads_digital = self.DRAM_input_reads_analog.astype(int)
+		self.DRAM_input_reads = self.DRAM_input_reads.astype(int)
 
 	def calculate_NN_totals(self):
-		#self.num_compute_clock_cycles_analog_total  = sum(self.num_compute_clock_cycles_analog)
-		#self.num_compute_clock_cycles_digital_total = sum(self.num_compute_clock_cycles_digital)
 		self.num_compute_clock_cycles_total = sum(self.num_compute_clock_cycles)
-
 		self.num_program_compute_instance_total     = sum(self.num_program_compute_instance)
 		self.num_program_clock_cycles_total         = sum(self.num_program_clock_cycles)
+		self.DRAM_input_reads_total  = sum(self.DRAM_input_reads)
 
-		self.SRAM_input_reads_total      = sum(self.SRAM_input_reads)
-		self.SRAM_filter_reads_total     = sum(self.SRAM_filter_reads)
-		self.SRAM_output_writes_SS_total = sum(self.SRAM_output_writes_SS)
 
-		self.DRAM_filter_reads_total     = sum(self.DRAM_filter_reads)
-		self.DRAM_output_writes_SS_total = sum(self.DRAM_output_writes_SS)
-
-		self.DRAM_input_reads_analog_total  = sum(self.DRAM_input_reads_analog)
-		self.DRAM_input_reads_digital_total = sum(self.DRAM_input_reads_digital)
-
-		self.SRAM_output_reads_acc_total  = sum(self.SRAM_output_reads_acc)
-		self.SRAM_output_writes_acc_total = sum(self.SRAM_output_writes_acc)
-		self.DRAM_output_reads_acc_total  = sum(self.DRAM_output_reads_acc)
-		self.DRAM_output_writes_acc_total = sum(self.DRAM_output_writes_acc)
-
-		self.DRAM_input_reads_digital_SRAM_sharing_total   = sum(self.DRAM_input_reads_digital_SRAM_sharing)
-		self.DRAM_output_writes_acc_SRAM_sharing_total     = sum(self.DRAM_output_writes_acc_SRAM_sharing)
-
-	def return_specs_SS_compare(self):
-		runspecs_names = ["SRAM Input Reads", "SRAM Filter Reads", "SRAM Output Writes", \
-			"DRAM Input Reads", "DRAM Filter Reads", "DRAM Output Writes", \
-			"Total Program/Compute Instances", "Total Programming Clock Cycles", \
-			"Total Compute Clock Cycles"]
+	def return_basic_results(self):
+		runspecs_names = ["DRAM Input Reads", "Total Program/Compute Instances", \
+					"Total Programming Clock Cycles", "Total Compute Clock Cycles"]
 	
-		totals = [self.SRAM_input_reads_total, self.SRAM_filter_reads_total, self.SRAM_output_writes_SS_total, \
-			self.DRAM_input_reads_digital_total, self.DRAM_filter_reads_total, self.DRAM_output_writes_SS_total, \
-			self.num_program_compute_instance_total, -1, \
-			self.num_compute_clock_cycles_total]
-
-		return(pd.DataFrame(totals, runspecs_names))
-	
-	def return_specs_self_compare(self):
-		# need to show effects of 1) accumulator modeling, 2) SRAM sharing
-		# 3) digital vs analog clock cycles, 4) digital vs analog DRAM input reads
-		runspecs_names = ["Total Compute Clock Cycles",\
-			"DRAM Input Reads Analog", "DRAM Input Reads Digital", \
-			"SRAM Output Writes SS", "DRAM Output Writes SS",\
-			"SRAM Output Writes Acc", "SRAM Output Reads Acc",\
-			"DRAM Output Writes Acc", "DRAM Output Reads Acc", \
-			"DRAM Input Reads Digital SRAM Sharing", "DRAM Output Writes Digital Acc SRAM Sharing"]
-		
-		totals = [self.num_compute_clock_cycles_total,\
-		self.DRAM_input_reads_analog_total, self.DRAM_input_reads_digital_total,\
-		self.SRAM_output_writes_SS_total, self.DRAM_output_writes_SS_total, \
-		self.SRAM_output_writes_acc_total, self.SRAM_output_reads_acc_total, \
-		self.DRAM_output_writes_acc_total, self.DRAM_output_reads_acc_total, \
-		self.DRAM_input_reads_digital_SRAM_sharing_total, self.DRAM_output_writes_acc_SRAM_sharing_total]
+		totals = [self.DRAM_input_reads_total, self.num_program_compute_instance_total, \
+			-1, self.num_compute_clock_cycles_total]
 
 		return(pd.DataFrame(totals, runspecs_names))
